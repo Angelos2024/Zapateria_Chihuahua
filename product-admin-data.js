a0620600dd7a47b89f8f56b8a9a2c361660b6d43
Angelos2024 <skyblackroan@gmail.com>
2026-05-08 16:02:29 -0600

    jhoi

--- product-admin-data.js
+++ product-admin-data.js
@@ -1,5 +1,5 @@
 window.PRODUCT_ADMIN_DATA = {
-  "savedAt": "2026-05-08T21:52:58.838Z",
+  "savedAt": "2026-05-08T22:01:50.538Z",
   "source": "product-admin",
   "products": {
     "inventory:botas:atx cafe": {
@@ -17,13 +17,23 @@ window.PRODUCT_ADMIN_DATA = {
     },
     "inventory:botas:atx negra": {
       "maxSize": 31,
-      "image": "img/productos/inventory-botas-atx-negra-1.jpg",
+      "image": "img/productos/inventory-botas-atx-negra-2.jpg",
       "images": [
         "img/productos/inventory-botas-atx-negra-1.jpg",
         "img/productos/inventory-botas-atx-negra-2.jpg",
         "img/productos/inventory-botas-atx-negra-3.jpg"
       ],
-      "coverImage": "img/productos/inventory-botas-atx-negra-1.jpg"
+      "coverImage": "img/productos/inventory-botas-atx-negra-2.jpg",
+      "details": [
+        "Modelo ATX Negro modelo Antiderrapante, Suela de Poliuretano.",
+        "Confeccionado bajo la NOM113-STPS-2009 Casco de Poliamida dieléctricos."
+      ]
+    },
+    "inventory:botas:axis 030 azul": {
+      "maxSize": 30,
+      "image": "",
+      "coverImage": "",
+      "images": []
     }
   }
 };
